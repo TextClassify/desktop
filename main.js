53d6fc27e8b686d834c7ba0e22a318a78c192aac
laoqiren <780649403@qq.com>
2018-03-25 14:36:49 +0800

    完善环境

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const url = require('url')
 
 function createWindow () {
 // Create the browser window.
-win = new BrowserWindow({width: 800, height: 600})
+win = new BrowserWindow({width: 800, height: 600});
 // 然后加载应用的 index.html。
 win.loadURL(url.format({
-        pathname: path.join(__dirname, 'build/index.html'),
-        protocol: 'file:',
+        pathname: process.env.NODE_ENV === "development" ? "localhost:3000":path.join(__dirname, 'build/index.html'),
+        protocol: 'http:',
         slashes: true
     }));
 }
