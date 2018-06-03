783d1de2411a5e9eb951e4efef23b265a857fb7a
laoqiren <780649403@qq.com>
2018-06-03 21:18:50 +0800

    完善环境

--- main.js
+++ main.js
@@ -3,14 +3,16 @@ const path = require('path')
 const url = require('url')
 
 function createWindow () {
-// Create the browser window.
-win = new BrowserWindow({width: 800, height: 600});
-// 然后加载应用的 index.html。
-win.loadURL(url.format({
-        pathname: process.env.NODE_ENV === "development" ? "localhost:3000":path.join(__dirname, 'build/index.html'),
-        protocol: 'http:',
-        slashes: true
-    }));
+    // Create the browser window.
+    win = new BrowserWindow({width: 800, height: 600});
+    win.webContents.openDevTools();
+
+    // 然后加载应用的 index.html。
+    win.loadURL(url.format({
+            pathname: process.env.NODE_ENV === "development" ? "localhost:3000":path.join(__dirname, 'build/index.html'),
+            protocol: 'http:',
+            slashes: true
+        }));
 }
 
 app.on('ready', createWindow);
--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -3,6 +3,27 @@ import process from 'process';
 import * as fs from "fs";
 import * as path from 'path';
 class Add extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            val: 0
+        }
+    }
+    componentDidMount(){
+        this.setState({val: this.state.val + 1});
+        console.log(this.state.val);    // 第 1 次 log
+
+        this.setState({val: this.state.val + 1});
+        console.log(this.state.val);    // 第 2 次 log
+
+        setTimeout(() => {
+        this.setState({val: this.state.val + 1});
+        console.log(this.state.val);  // 第 3 次 log
+
+        this.setState({val: this.state.val + 1});
+        console.log(this.state.val);  // 第 4 次 log
+        }, 0);
+    }
     selectFile(){
         fs.readFile(path.join(process.cwd(),'src/components/Home/Home.js'),(err,data)=>{
             if(err) return;
