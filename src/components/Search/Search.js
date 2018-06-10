3438c1afcc7599a508d29ef653c354b5277d75c8
laoqiren <780649403@qq.com>
2018-06-11 00:11:59 +0800

    搜索功能

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
 import { Input } from 'antd';
+import List from '../List/List';
 const Search = Input.Search;
 
+const fs = require('fs');
+const csv = require('csvtojson');
+
+const { toLocalStorage, toLocalStorageContent } = require('../../config');
+
 class SearchComponent extends Component {
-    handleSearch(){
-        console.log("正在开发中，敬请期待！")
+    constructor(props){
+        super(props);
+        this.state = {
+            data: [],
+            searchResult: []
+        }
+    }
+    componentDidMount(){
+        csv()
+            .fromFile(toLocalStorage)
+            .then(jsonObj=>{
+                this.setState({
+                    data: jsonObj
+                });
+            });
+    }
+    handleSearch(value){
+        let result = this.state.data.filter(item=>{
+            return RegExp(value).test(item.path);
+        });
+        this.setState({
+            searchResult: result
+        });
     }
     render(){
         return (
@@ -12,10 +39,13 @@ class SearchComponent extends Component {
                 <h2>搜索内容</h2>
                 <Search
                 placeholder="搜索内容"
-                onSearch={()=>this.handleSearch()}
+                onSearch={value=>this.handleSearch(value)}
                 size="large"
                 enterButton
                 />
+                {
+                    this.state.searchResult.length > 0 && <List data={this.state.searchResult} title="搜索结果"/>
+                }
             </div>
         )
     }
